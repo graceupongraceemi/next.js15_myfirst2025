473474e2f42a0c5226b056301660ac1366dfcd41
Emiko Esther S <graceupongraceemi@gmail.com>
2025-01-06 05:48:54 -0800

    components holder and hello.tsx added in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-// import Image from 'next/image';
+import Hello from '@/app/components/hello';
 
 export default function Home() {
-  console.log('What am I doing here? -- SERVER/CLIENT');
-  return <h1 className='text-3xl'>Welcome to Next.js</h1>;
+  console.log('What am I doing here? -- SERVER');
+
+  return (
+    <>
+      <h1 className='text-3xl'>Welcome to Next.js</h1>;
+      <Hello />
+    </>
+  );
 }
 
 // export default function Home() {
