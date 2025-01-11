a3d895d0e682cf9a26e00d2b1365df917617bbad
Emiko Esther S <graceupongraceemi@gmail.com>
2025-01-11 15:19:20 -0800

    NAVBAR added to layout.tsx in root folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const layout = ({ childre }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      l<h1 className='text-3xl'>NAVBAR</h1>
+    </div>
+  );
+};
+
+export default layout;
