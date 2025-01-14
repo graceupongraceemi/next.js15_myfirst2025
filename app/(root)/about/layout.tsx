ff5071c4810d6734abae036a48203ebc58406101
Emiko Esther S <graceupongraceemi@gmail.com>
2025-01-13 19:38:32 -0800

    In layout.ts of about folder, added children in return div

--- app/(root)/about/layout.tsx
+++ app/(root)/about/layout.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      l<h1 className='text-3xl'>NAVBAR</h1>
+      <h1 className='text-3xl'>NAVBAR</h1>
+      {children}
     </div>
   );
 };
